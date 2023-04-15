c70cba4e5f949d835cfd7db2d48bd5797288e60f
bjarte <22171847+freebattie@users.noreply.github.com>
2023-04-15 18:12:38 +0200

    started porting over old classes

---
+++ src/arduino_secrets.h
@@ -0,0 +1,10 @@
+#define NAME "edge-01"
+#define SECRET_SSID  "Phonex"
+#define SECRET_PASS  "Powerme33"
+#define HTTP_SERVER "172.20.10.8"
+#define HTTP_PORT 3000
+#define MQTT_HOST IPAddress(172, 20, 10, 8)
+#define MQTT_PORT 4000
+#define MQTT_PASSWORD "bjkajdks"
+
+#define FW_BUILD "dev";
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,78 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+#include <ArduinoJson.h>
+#include "timer.h"
+#include "arduino_secrets.h"
+#include <WiFi.h>
+#include <AsyncMqttClient.h>
+#include "ota.h"
+#include "mqtt.h"
+#include "storage.h"
+#include <Arduino.h>
+
+#define HALL_SENSOR_PIN 1
+
+bool isDoorOpen = false;
+
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+Timer wifiTimer = Timer();
+Timer mqttTimer = Timer();
+Ota wifiOta = Ota();
+Storage store = Storage();
+
 
 void setup() {
   // put your setup code here, to run once:
+   pinMode(HALL_SENSOR_PIN, INPUT);
+  Serial.begin(9800);
+  delay(4000);
+
+  store.start();
+
+  profile_t profile = store.getProfile();
+
+ 
+  setupTimers();
+
+  //mqttConnect.setup();
+  delay(1000);
+  wifiOta.connectWiFi();
+  delay(1000);
+  Serial.println("SETUP DONE");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+void setupTimers()
+{
+ 
+  wifiTimer.setInterval(5000);
+  wifiTimer.start();
+
+  mqttTimer.setInterval(5000);
+  mqttTimer.start();
+}
+void handelConnections()
+{
+  if (wifiOta.status() != WL_CONNECTED && wifiTimer.checkInterval() == RUNCODE)
+  {
+    wifiTimer.reset();
+    wifiOta.connectWiFi();
+  }
+  else if (!mqttConnect.connected() && wifiOta.status() == WL_CONNECTED && mqttTimer.checkInterval() == RUNCODE)
+  {
+    mqttTimer.reset();
+    Serial.println("Restarting device standby .. ");
+    delay(2000);
+    ESP.restart();
+  }
+  else if (!mqttConnect.connected() && wifiOta.status() == WL_CONNECTED)
+  {
+    Serial.println("dissconnected from mqtt.. ");
+    delay(200);
+  }
 }
